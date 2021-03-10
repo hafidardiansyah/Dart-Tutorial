81a3ae01e766a49c099b5cf75029bafb374f2cb1
Hafid Ardiansyah <62227964+hafidardiansyah@users.noreply.github.com>
2021-03-10 13:40:21 +0700

    add property

--- oop/main.dart
+++ oop/main.dart
@@ -1,24 +1,10 @@
+import 'kompleks.dart';
+
 void main() {
   Kompleks kompleks = Kompleks();
 
   kompleks.namaRumah = 'Rumah hafid';
-  Kompleks.nomerRumah = 77;
+  kompleks.nomerRumah = 77;
 
   print(kompleks.identitas());
-}
-
-class Kompleks {
-  // field
-  final namaKompeks = 'Hafid';
-  late String namaRumah;
-  static int nomerRumah = 0;
-
-  // method / fungsi
-  String identitas() {
-    return this.namaRumah +
-        ' ' +
-        nomerRumah.toString() +
-        ' Kompleks ' +
-        namaKompeks;
-  }
 }
